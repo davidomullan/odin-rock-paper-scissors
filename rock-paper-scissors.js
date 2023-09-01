925e0a3c2a3490e20f7f5313e56a22a36c34acf1
David O'Mullan <35239651+davidomullan@users.noreply.github.com>
2023-09-01 09:42:49 -0500

    Add GUI to play game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,84 +1,100 @@
-// Round of Rock Paper Scissors
-function round() {
-	let userChoice, botChoice;
+// Reset selection styling prior to next game
+function resetHighlight(){
+	const choices = document.querySelectorAll('button');
+	choices.forEach((choice) => {
+	  choice.classList.remove('selected');
+	});
+}
 
-	while(true){
-		userChoice = prompt("Rock 🪨 = 1, Paper 📰 = 2, Scissors ✄ = 3. \nEnter your choice: ");
-		if (userChoice == 1 || userChoice == 2 || userChoice ==3){
-			break;
-		}
-		else {
-			alert("Invalid Entry! Try again");
-		}
-	}
+// Round of Rock Paper Scissors
+function round(userChoice) {
+	let botChoice;
+	const playerScore = document.querySelector('#playerScore');
+	const botScore = document.querySelector('#botScore');
+
+	resetHighlight();
 
 	// Bot selection between 1 and 3 inclusive
 	// Rock=1, Paper=2, Scissors=3
 	botChoice = Math.floor((Math.random() * 3)) + 1;
 
 	if (userChoice==1){
+		document.querySelector('#playerRock').classList.toggle('selected');
 		if (botChoice==2){
-			alert("User 🪨 | 📰 Bot");
-			return "bot";
+			document.querySelector('#botPaper').classList.toggle('selected');
+			botScore.textContent++;
 		}
 		else if (botChoice==3){
-			alert("User 🪨 | ✄ Bot");
-			return "user";
+			document.querySelector('#botScissors').classList.toggle('selected');
+			playerScore.textContent++;
 		}
 		else {
-			alert("Both chose 🪨");
-			return "draw";
+			document.querySelector('#botRock').classList.toggle('selected');
 		}
 	}
 	else if (userChoice==2){
+		document.querySelector('#playerPaper').classList.toggle('selected');
 		if (botChoice==3){
-			alert("User 📰 | ✄ Bot");
-			return "bot";
+			document.querySelector('#botScissors').classList.toggle('selected');
+			botScore.textContent++;
 		}
 		else if (botChoice==1){
-			alert("User 📰 | 🪨 Bot");
-			return "user";
+			document.querySelector('#botRock').classList.toggle('selected');
+			playerScore.textContent++;
 		}
 		else {
-			alert("Both chose 📰");
-			return "draw";
+			document.querySelector('#botPaper').classList.toggle('selected');
 		}
 	}
 	else {
+		document.querySelector('#playerScissors').classList.toggle('selected');
 		if (botChoice==1){
-			alert("User ✄ | 🪨 Bot");
-			return "bot";
+			document.querySelector('#botRock').classList.toggle('selected');
+			botScore.textContent++;
 		}
 		else if (botChoice==2){
-			alert("User ✄ | 📰 Bot");
-			return "user";
+			document.querySelector('#botPaper').classList.toggle('selected');
+			playerScore.textContent++;
 		}
 		else {
-			alert("Both chose ✄");
-			return "draw";
+			document.querySelector('#botScissors').classList.toggle('selected');
 		}
+	}
+
+	// Win Condition
+	if (playerScore.textContent == "5"){
+		alert("Player wins!");
+		playerScore.textContent = "0";
+		botScore.textContent = "0";
+		resetHighlight();
+	}
+	else if(botScore.textContent == "5"){
+		alert("Bot wins!");
+		playerScore.textContent = "0";
+		botScore.textContent = "0";
+		resetHighlight();
 	}
 }
 
-// Play game of Rock Paper Scissors
-// 5 rounds
-function game(){
-	let userScore = 0, botScore = 0;
-	while(userScore<5 && botScore <5){
-		let result = round();
-		if(result=="user"){
-			userScore++;
-		}
-		else if(result=="bot"){
-			botScore++;
-		}
-		alert(`User Score: ${userScore}, Bot Score: ${botScore}`);
-	}
+// // Play game of Rock Paper Scissors
+// // 5 rounds
+// function game(){
+// 	let userScore = 0, botScore = 0;
+// 	while(userScore<5 && botScore <5){
+// 		let result = round();
+// 		if(result=="user"){
+// 			userScore++;
+// 		}
+// 		else if(result=="bot"){
+// 			botScore++;
+// 		}
+// 		alert(`User Score: ${userScore}, Bot Score: ${botScore}`);
+// 	}
 
-	if (userScore > botScore){
-		alert("User wins!");
-	}
-	else {
-		alert("Bot wins..");
-	}
-}
+// 	if (userScore > botScore){
+// 		alert("User wins!");
+// 	}
+// 	else {
+// 		alert("Bot wins..");
+// 	}
+// }
